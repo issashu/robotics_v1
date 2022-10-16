1a86bb927931039edf549660bc24a6d57d9fd1a6
Iordan Tonchev <issashu@icloud.com>
2022-10-16 11:16:16 +0300

    folder and gitignore sorting

---
+++ src/robo_miner/robo_miner_gui/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/parallels/CLionProjects/Homeworks/robotics_v1/src/robo_miner/robo_miner_gui
+# Build directory: /home/parallels/CLionProjects/Homeworks/robotics_v1/src/robo_miner/robo_miner_gui/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
