120482c589a924f2b05fc200b5023c4eadbd912d
Iordan Tonchev <issashu@icloud.com>
2022-10-28 15:27:53 +0300

    OK finally working vector of commands to robot..

--- src/robo_miner/robo_miner_controller/src/main.cpp
+++ src/robo_miner/robo_miner_controller/src/main.cpp
@@ -1,6 +1,12 @@
 #include "rclcpp/rclcpp.hpp"
 #include "MovementController.h"
 
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
 /**
  * @brief Uses a vector of moves to send to the robot. If you need just one move, pass a 1 elem vector :)
  * 
@@ -8,6 +14,7 @@
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
     std::vector<std::shared_ptr<RoboMoveClientNode>> movesList;
+    sleep(5);
 
     auto node = std::make_shared<RoboMoveClientNode>("Node", MoveType::FORWARD);
     auto node1 = std::make_shared<RoboMoveClientNode>("Node1", MoveType::ROTATE_RIGHT);
@@ -19,8 +26,15 @@ int main(int argc, char **argv) {
     movesList.push_back(node2);
     movesList.push_back(node3);
 
-    for(std::shared_ptr<RoboMoveClientNode> item : movesList){
-        rclcpp::spin_until_future_complete(item, item->CallRoboMoveService());
+    for(std::shared_ptr<RoboMoveClientNode> item : movesList) {
+        if (rclcpp::spin_until_future_complete(item, item->CallRoboMoveService()) == rclcpp::FutureReturnCode::SUCCESS){
+            sleep(2);
+            continue;
+        }
+        else{
+            printf("I am missing a logger! Yay!");
+
+        }
     }
     
     rclcpp::shutdown();
